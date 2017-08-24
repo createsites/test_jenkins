e024e1c254601674b14552d9c724a2e63dd5f2b0
a.novoselov <admin@createsites.net>
2017-08-24 13:53:20 +0700

    Removed failure test.

--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -14,8 +14,8 @@ class UsersTest extends TestCase
 	$this->assertEquals(true, true);
     }
 
-    public function testFailure()
+    /*public function testFailure()
     {
 	$this->assertEquals(true, false);
-    }
+    }*/
 }
